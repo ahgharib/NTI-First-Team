762bd712da6bdd0fdd8534feea3319babc5cf226
ahgharib <ahgharib1643@gmail.com>
2025-11-15 14:22:19 +0200

    Making API Part1

--- First Project/app.py
+++ First Project/app.py
@@ -1,1 +1,19 @@
 from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(
+    title="🚀 FastAPI Heart Disease",
+    description="Heart Disease Model in NTI",
+    version="1.0.0"
+)
+
+@app.get("/")
+async def root():
+    return {"message": "API WORKING"}
+
+
+@app.post("/Parameters/")
+async def get_parameters():
+    pass
+    # return parameters
